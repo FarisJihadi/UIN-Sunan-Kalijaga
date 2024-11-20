8f58ef01135837f933f25c3649b6610bc422f804
FarisJihadi <23106050031@student.uin-suka.ac.id>
2024-11-20 20:41:34 +0700

    Reupload file praktikum 1 dev javascript
    ulang tadi kurang rapi

---
+++ Semester-3/Konsep-Bahasa-Pemrograman/Dev-JavaScript/Praktikum_1/expression-&-statemen.js
@@ -0,0 +1,9 @@
+// expression
+const age = 10;
+const name = "Dicoding";
+console.log(`Aku ${name}, umurku ${age} tahun`);
+
+// statement
+const umur = 10;
+const nama = "Dicoding";
+console.log(`Aku ${nama}, umurku ${umur} tahun`);
---
+++ Semester-3/Konsep-Bahasa-Pemrograman/Dev-JavaScript/Praktikum_1/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+// mengubah number ke string secara implisit
+const age = 20;
+const message = "umurku: " + age;
+console.log(message); // umurku: 20
