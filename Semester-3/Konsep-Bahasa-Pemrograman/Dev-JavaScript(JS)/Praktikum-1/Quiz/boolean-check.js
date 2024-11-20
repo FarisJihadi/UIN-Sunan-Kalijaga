70ff07c6ceb37e62d06062dbd85683381e13d169
FarisJihadi <23106050031@student.uin-suka.ac.id>
2024-11-20 20:38:36 +0700

    upload quiz praktikum 1 dev javascript

---
+++ Semester-3/Konsep-Bahasa-Pemrograman/Dev-JavaScript(JS)/Praktikum-1/Quiz/boolean-check.js
@@ -0,0 +1,19 @@
+const number = 123;
+const string = "Dicoding";
+const empty = null;
+
+const boolFromNum = Boolean(123);
+const boolFromStr = Boolean(string);
+const boolFromNull = Boolean(empty);
+
+console.log(boolFromNum); // true
+console.log(typeof boolFromNum); // boolean
+
+console.log(boolFromStr); // true
+console.log(typeof boolFromStr); // boolean
+
+console.log(boolFromNum); // false
+console.log(typeof boolFromNum); // boolean
+
+console.log(boolFromNull); // false\
+console.log(typeof boolFromNull); // boolean
---
+++ Semester-3/Konsep-Bahasa-Pemrograman/Dev-JavaScript(JS)/Praktikum-1/Quiz/string.js
@@ -0,0 +1,11 @@
+// double quotes section
+let doubleQuotes = "Belajar JavaScript bersama Asepp";
+console.log(doubleQuotes);
+
+// single quotes section
+let singleQuotes = "Belajar JavaScript bersama Asepp";
+console.log(singleQuotes);
+
+// backticks section
+let kalimatbakTicks = `Belajar Javascript bersama Asepp`;
+console.log(kalimatbakTicks);
