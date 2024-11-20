1fe2b6fa13c00505f6a236d15c62e9341c910869
FarisJihadi <23106050031@student.uin-suka.ac.id>
2024-11-20 20:43:01 +0700

    Reupload Quiz Praktikum 1 Dev JavaScript
    Ulang tadi berantakan.

---
+++ Semester-3/Konsep-Bahasa-Pemrograman/Dev-JavaScript/Praktikum_1/Quiz/implisit.js
@@ -0,0 +1,11 @@
+// // Jika menggunakan operator * pada string dan num
+// const strNumber = "123";
+// const result = strNumber * 2;
+
+// console.log(result); // Output: 246
+
+// Jika menggunakan operator + pada num dan bool
+const bool = true;
+const result = 1 + bool;
+
+console.log(result); // Output: 2
---
+++ Semester-3/Konsep-Bahasa-Pemrograman/Dev-JavaScript/Praktikum_1/Quiz/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+// Jika menggunakan operator * pada string dan num
+const strNumber = "123";
+const result = strNumber * 2;
+
+console.log(result); // Output: 246
